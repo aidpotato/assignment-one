ea48c96cf5fc03316dbf76973f658efa730f3e5c
AIDAN <dr.hammer1234@gmail.com>
2023-04-04 20:35:17 -0700

    added jave to reshow wlement when new not is clicked

--- java/javascript.js
+++ java/javascript.js
@@ -20,35 +20,21 @@ button.addEventListener('click', dark_mode)
 const cancel_button = document.querySelector('#cancel_button')
 
 function story_hide(){
-    console.log('monkey')
-    var hstory = document.getElementById('story', 'cancel_button', 'save_button')
-    if (hstory.style.display === "none") {
-        hstory.style.display = "block";
-      } else {
-        hstory.style.display = "none";
-      }
+  console.log('monkey')
+  var hstory = document.getElementById('story')
+  hstory.style.display = "none";
 }
 
 function save_hide(){
-    console.log('monkey')
-    var hsave= document.getElementById('save_button')
-    if (hsave.style.display === "none") {
-        hsave.style.display = "block";
-      } else {
-        hsave.style.display = "none";
-      }
-    
+  console.log('monkey')
+  var hsave= document.getElementById('save_button')
+  hsave.style.display = "none";    
 }
 
 function cancel_hide(){
-    console.log('monkey')
-    var hcancel = document.getElementById('cancel_button')
-    if (hcancel.style.display === "none") {
-        hcancel.style.display = "block";
-      } else {
-        hcancel.style.display = "none";
-      }
-    
+  console.log('monkey')
+  var hcancel = document.getElementById('cancel_button')
+  hcancel.style.display = "none";
 }
 cancel_button.addEventListener('click', cancel_hide)
 cancel_button.addEventListener('click', story_hide)
@@ -59,9 +45,31 @@ cancel_button.addEventListener('click', save_hide)
 
 const newNote_Text = document.querySelector('#NewNote_button')
 
+function showCancelbutton(){
+  var hcancel = document.getElementById('cancel_button');
+  hcancel.style.display = "block";
+}
+
+
+function showSavebutton(){
+  var hcancel = document.getElementById('save_button');
+  hcancel.style.display = "block";
+}
+
+
+function showStorybutton(){
+  var hcancel = document.getElementById('story');
+  hcancel.style.display = "block";
+}
+
+
 function newNote_removetext(){
   const textarea = document.querySelector('#story');
   textarea.value = '';
 
 }
+
+newNote_Text.addEventListener('click', showCancelbutton)
+newNote_Text.addEventListener('click', showSavebutton)
+newNote_Text.addEventListener('click', showStorybutton)
 newNote_Text.addEventListener('click', newNote_removetext)
