d193bae1fc48bb6ebbdb0a99721c6557c70af330
AIDAN <dr.hammer1234@gmail.com>
2023-04-04 19:49:25 -0700

    got the text to be reomved when new note is presed

--- java/javascript.js
+++ java/javascript.js
@@ -21,35 +21,47 @@ const cancel_button = document.querySelector('#cancel_button')
 
 function story_hide(){
     console.log('monkey')
-    var x = document.getElementById('story', 'cancel_button', 'save_button')
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    var hstory = document.getElementById('story', 'cancel_button', 'save_button')
+    if (hstory.style.display === "none") {
+        hstory.style.display = "block";
       } else {
-        x.style.display = "none";
+        hstory.style.display = "none";
       }
 }
 
 function save_hide(){
     console.log('monkey')
-    var x = document.getElementById('save_button')
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    var hsave= document.getElementById('save_button')
+    if (hsave.style.display === "none") {
+        hsave.style.display = "block";
       } else {
-        x.style.display = "none";
+        hsave.style.display = "none";
       }
     
 }
 
 function cancel_hide(){
     console.log('monkey')
-    var x = document.getElementById('cancel_button')
-    if (x.style.display === "none") {
-        x.style.display = "block";
+    var hcancel = document.getElementById('cancel_button')
+    if (hcancel.style.display === "none") {
+        hcancel.style.display = "block";
       } else {
-        x.style.display = "none";
+        hcancel.style.display = "none";
       }
     
 }
 cancel_button.addEventListener('click', cancel_hide)
 cancel_button.addEventListener('click', story_hide)
 cancel_button.addEventListener('click', save_hide)
+
+
+//  new note//
+
+const newNote_Text = document.querySelector('#NewNote_button')
+
+function newNote_removetext(){
+  const textarea = document.querySelector('#story');
+  textarea.value = '';
+
+}
+newNote_Text.addEventListener('click', newNote_removetext)
